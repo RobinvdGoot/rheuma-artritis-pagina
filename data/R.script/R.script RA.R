45b0506c9d55a2fbec42575659bca3a0a49ec838
RobinvdGoot <robin.van.der.goot@student.nhlstenden.com>
2026-05-28 16:34:35 +0200

    Update R.script RA.R

--- data/R.script/R.script RA.R
+++ data/R.script/R.script RA.R
@@ -65,6 +65,9 @@ dds_casus <- DESeqDataSetFromMatrix(countData = RA_casus,
                               design = ~ treatment)
 dds_casus <- DESeq(dds_casus)
 resultaten <- results(dds_casus)
+write.csv(RA_casus,
+          file = ,
+          row.names = TRUE)
 write.table(resultaten, file = 'cassus_RA123', row.names = TRUE, col.names = TRUE)
 sum(resultaten$padj < 0.05 & resultaten$log2FoldChange > 1, na.rm = TRUE)
 sum(resultaten$padj < 0.05 & resultaten$log2FoldChange < -1, na.rm = TRUE)
@@ -98,11 +101,12 @@ BiocManager::install("geneLenDataBase")
 BiocManager::install("org.Dm.eg.db")
 library("goseq")
 library("geneLenDataBase")
+
 ALL=rownames(resultaten)
 res<-as.data.frame(resultaten)
 DEG=res %>% 
   filter(padj<0.05)
-DEG1=rownames(DEG)
+DEG=rownames(DEG)
 #gene factor maken
 gene.vector=as.integer(ALL%in%DEG)
 names(gene.vector)=ALL
@@ -111,120 +115,6 @@ head(gene.vector)
 tail(gene.vector)
 #pwf
 pwf=nullp(gene.vector,"hg19","geneSymbol")
-#Go.waLl
-GO.wall=goseq(pwf,"hg19","geneSymbol")
-
-#How many enriched GO terms do we have
-class(GO.wall)
-head(GO.wall)
-nrow(GO.wall)
-enriched.GO=GO.wall$category[GO.wall$over_represented_pvalue<.05]
-#NOTE: They recommend using a more stringent multiple testing corrected p value here
-
-#How many GO terms do we have now?
-class(enriched.GO)
-head(enriched.GO)
-length(enriched.GO)
-library(GO.db)
-capture.output(for(go in enriched.GO[1:258]) { print(GOTERM[[go]])
-  cat("--------------------------------------\n")
-}
-, file="SigGo.txt")
-top10<- GO.wall %>% arrange(over_represented_pvalue) %>%
-slice(1:10)
-# ggplot 1.0
-ggplot(top10, aes(x = reorder(category, -over_represented_pvalue),
-                  y = -log10(over_represented_pvalue))) +
-  geom_col(fill = "steelblue") +
-  coord_flip() +
-  labs(
-    title = "Top 10 meest verrijkte GO‑categorieën",
-    x = "GO‑categorie",
-    y = "-log10(p‑waarde)"
-  ) +
-  theme_minimal()
-#ggplot 2.0
-ggplot(top10, aes(x = -log10(over_represented_pvalue),
-                  y = reorder(category, over_represented_pvalue))) +
-  geom_point(size = 4, color = "darkred") +
-  labs(
-    title = "Top 10 GO‑categorieën (dotplot)",
-    x = "-log10(p‑waarde)",
-    y = "GO‑categorie"
-  ) +
-  theme_minimal()
-#ggplot 3.0
-ggplot(top10,
-       aes(x = reorder(category, over_represented_pvalue),
-           y = -log10(over_represented_pvalue),
-           fill = -log10(over_represented_pvalue))) +
-  
-  geom_col(width = 0.8) +
-  
-  coord_flip() +
-  
-  scale_fill_gradient(
-    low = "skyblue",
-    high = "darkblue"
-  ) +
-  
-  labs(
-    title = "Top 10 meest verrijkte GO-categorieën",
-    subtitle = "Over-representation analyse",
-    x = "GO-categorie",
-    y = expression(-log[10](pwaarde)),
-    fill = expression(-log[10](pwaarde))
-  ) +
-  
-  theme_minimal(base_size = 13) +
-  
-  theme(
-    panel.grid.major.y = element_blank(),
-    legend.position = "right",
-    plot.title = element_text(face = "bold")
-  )
-# KEGG maken
-BiocManager::install("clusterProfiler")
-library(clusterProfiler)
-library(org.Hs.eg.db)
-library(dplyr)
-library(stringr)
-  
-
-  library(org.Hs.eg.db)
-  library(clusterProfiler)
-  
-  gene_conversion <- bitr(
-    res_df$ENSEMBL,
-    fromType = "SYMBOL",
-    toType = "ENTREZID",
-    OrgDb = org.Hs.eg.db )
-  res_df <- as.data.frame(resultaten)
-  
-  res_df$SYMBOL <- rownames(res_df)
-  
-  res_annotated <- merge(
-    res_df,
-    gene_conversion,
-    by = "SYMBOL"
-  )
-  gene_list <- res_annotated$log2FoldChange
-  
-  names(gene_list) <- res_annotated$ENTREZID 
-  pathview(
-    gene.data = resultaten,
-    pathway.id = "hsa05323",
-    species = "hsa",
-    gene.idtype = "SYMBOL",     
-  )
-  sig_genes <- res_annotated %>%
-    filter(padj < 0.05 & abs(log2FoldChange) > 1)
-  kegg <- enrichKEGG(
-    gene = sig_genes$SYMBOL,
-    organism = "hsa"
-  )
-  
-  dotplot(kegg)
   pathview(
     gene.data = resultaten,
     pathway.id = "hsa04010",  
